4193023156626090cb0c09a198a97abeaaf01d7d
Abdul Ahad <99830052+AbdulAhadilyas@users.noreply.github.com>
2022-10-12 20:16:52 +0500

    still working

--- src/component/right.jsx
+++ src/component/right.jsx
@@ -1,60 +1,22 @@
-
-import './css/right.css'
-const Right = () => {
-    return (
-        <div className="main-right-box">
-            <div className="add">
-                <h3>Sponsored</h3>
-                <div className="addimg">
-                <img src="https://scontent.fkhi2-2.fna.fbcdn.net/v/t45.1600-4/306788715_23851808771390274_5837086775338139860_n.png?stp=cp0_dst-jpg_p296x100_q90_spS444&_nc_cat=1&ccb=1-7&_nc_sid=67cdda&_nc_ohc=7nutKak3IakAX-Sb-kF&_nc_ht=scontent.fkhi2-2.fna&oh=00_AT8j45_CjDXekP_5rTeBoQZ0D5oKMUfbidsJkZy6w5iELw&oe=634B16FB"
-                 alt="" />
-                 <span>
-                 The Rivalry Calls For you
-                 </span>
-                 <small>mcdelivery.com.pk</small>
-                 </div>
-            </div>
-
-            <div className="right1">
-            <p className='p1'>Birthdays</p>
-            <div className='bd2'>
-                <img src="./img/1.png" alt="" />
-                <p>Shayan Malik's <span>birthday is today</span> </p>
-            </div>
-            <hr />
-            <p className='p'>Your Pages and Profiles</p>
-            <div className='pages'>
-            
-                <p>1 Notifications</p>
-            </div>
-            <hr />
-            <p className='p'>Contacts</p>
-            <div className='contacts'>
-                <div>
-                    <img src="https://scontent.fkhi2-3.fna.fbcdn.net/v/t39.30808-1/296947818_610871363935919_850577250435750206_n.jpg?stp=c0.0.40.40a_cp0_dst-jpg_p40x40&_nc_cat=104&ccb=1-7&_nc_sid=7206a8&_nc_ohc=iVQtUX7JdOoAX997oFk&_nc_ht=scontent.fkhi2-3.fna&oh=00_AT8qTzNxii-T-1hNdvKGcciLpFYh6XTjSOAs7FNvkvQvyQ&oe=634ACE03" alt="https://scontent.fkhi2-3.fna.fbcdn.net/v/t39.30808-1/296947818_610871363935919_850577250435750206_n.jpg?stp=c0.0.40.40a_cp0_dst-jpg_p40x40&_nc_cat=104&ccb=1-7&_nc_sid=7206a8&_nc_ohc=iVQtUX7JdOoAX997oFk&_nc_ht=scontent.fkhi2-3.fna&oh=00_AT8qTzNxii-T-1hNdvKGcciLpFYh6XTjSOAs7FNvkvQvyQ&oe=634ACE03" />
-                    <p>Usman Matloob</p>
-                </div>
-                <div>
-                    <img src="https://scontent.fkhi2-3.fna.fbcdn.net/v/t39.30808-1/296947818_610871363935919_850577250435750206_n.jpg?stp=c0.0.40.40a_cp0_dst-jpg_p40x40&_nc_cat=104&ccb=1-7&_nc_sid=7206a8&_nc_ohc=iVQtUX7JdOoAX997oFk&_nc_ht=scontent.fkhi2-3.fna&oh=00_AT8qTzNxii-T-1hNdvKGcciLpFYh6XTjSOAs7FNvkvQvyQ&oe=634ACE03" alt="" />
-                    <p> Anas Khan</p>
-                </div>
-                <div>
-                    <img src="https://scontent.fkhi2-3.fna.fbcdn.net/v/t39.30808-1/296947818_610871363935919_850577250435750206_n.jpg?stp=c0.0.40.40a_cp0_dst-jpg_p40x40&_nc_cat=104&ccb=1-7&_nc_sid=7206a8&_nc_ohc=iVQtUX7JdOoAX997oFk&_nc_ht=scontent.fkhi2-3.fna&oh=00_AT8qTzNxii-T-1hNdvKGcciLpFYh6XTjSOAs7FNvkvQvyQ&oe=634ACE03" alt="" />
-                    <p> Fahad Paracha</p>
-                </div><div>
-                    <img src="https://scontent.fkhi2-3.fna.fbcdn.net/v/t39.30808-1/296947818_610871363935919_850577250435750206_n.jpg?stp=c0.0.40.40a_cp0_dst-jpg_p40x40&_nc_cat=104&ccb=1-7&_nc_sid=7206a8&_nc_ohc=iVQtUX7JdOoAX997oFk&_nc_ht=scontent.fkhi2-3.fna&oh=00_AT8qTzNxii-T-1hNdvKGcciLpFYh6XTjSOAs7FNvkvQvyQ&oe=634ACE03" alt="" />
-                    <p> Mahad Siddiqui</p>
-                </div><div>
-                    <img src="https://scontent.fkhi2-3.fna.fbcdn.net/v/t39.30808-1/296947818_610871363935919_850577250435750206_n.jpg?stp=c0.0.40.40a_cp0_dst-jpg_p40x40&_nc_cat=104&ccb=1-7&_nc_sid=7206a8&_nc_ohc=iVQtUX7JdOoAX997oFk&_nc_ht=scontent.fkhi2-3.fna&oh=00_AT8qTzNxii-T-1hNdvKGcciLpFYh6XTjSOAs7FNvkvQvyQ&oe=634ACE03" alt="" />
-                    <p> Sami Khan</p>
+import React from 'react'
+import "./css/right.css"
+export default function right() {
+  return (
+    <div>
+        <div className='right-main'>
+       
+            <h2 className='sposor'>Sponsored</h2>
+            <div className="top-add">
+                <div className='add-img'>
+                    <img src="https://scontent.fkhi2-3.fna.fbcdn.net/v/t45.1600-4/305847308_23851808738800274_6764102293586775687_n.png?stp=cp0_dst-jpg_p296x100_q90_spS444&_nc_cat=106&ccb=1-7&_nc_sid=67cdda&_nc_ohc=IgZEYRAgxtIAX-xGSTb&tn=Bxtp1R5VRVkCOp_6&_nc_ht=scontent.fkhi2-3.fna&oh=00_AT_Eda6qmjwfft9nPo9oW5ZcmWN5aq766Kqk-IliMDureA&oe=634B45C5" alt="" />
+                    <div className="txt">
+                    <span className='add-span'>Support your fave flavor</span>
+                    <div className='add-span1'>burgeroclock.com.pk</div>
+                    </div>
                 </div>
             </div>
-
-
-
-
+        <hr  className='addline'/>
         </div>
-        </div>
-    )
+    </div>
+  )
 }
-export default Right;

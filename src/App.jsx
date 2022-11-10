ac97266b3a42f84f34b1c5b5559945c3837ed714
Abdul Ahad <99830052+AbdulAhadilyas@users.noreply.github.com>
2022-11-10 20:46:40 +0500

    add auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,68 @@
-import Nav from './component/nav'
-import Home from './component/pages/Home';
-import Router from './component/Routers/Router';
+
+import { useState } from 'react';
+import From from './component/Form';
+import { initializeApp } from "firebase/app"
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPqtZOv212keJhpLWnuTTWVScLtiaFW3w",
+  authDomain: "fakeb00k-01.firebaseapp.com",
+  projectId: "fakeb00k-01",
+  storageBucket: "fakeb00k-01.appspot.com",
+  messagingSenderId: "810198591603",
+  appId: "1:810198591603:web:a049dea4189b1a63c86206"
+};
 
 
 
 function App() {
+  const app = initializeApp(firebaseConfig);
+ 
+  const [email,setEmail] =useState()
+  const [password,setPassword] =useState()
+
+
+  const createAccount =  (e) => {
+    e.preventDefault()
+    const auth = getAuth(app);
+    createUserWithEmailAndPassword(auth, email,password )
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+        console.log(user)
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+    console.log(email,password)
+   
+  }
+
+ 
+
+
 
   return (
     <div>
-      <div className="nav">
+      {/* <div className="nav">
         <Nav />
       </div>
-      <Router/>
+      <Router/> */}
+
+      <From  getUserEmail={(e)=>setEmail(e.target.value)} 
+       getUserPass={(e)=>setPassword(e.target.value)}
+       createAccount={createAccount}
+      
+      
+      />
+
     </div>
 
 
-    // <div className="right-fixd">
-    //   <Right />
-    // </div>
+
   );
 }
 
---
+++ src/component/Form.jsx
@@ -0,0 +1,216 @@
+import React from 'react'
+import "./css/Form.css"
+import { useState } from 'react'
+
+
+
+export default function From({getUserEmail ,getUserPass,createAccount}) {
+    const [show, setShow] = useState(false)
+
+
+
+
+    const handleShow = () => {
+        setShow(!show);
+        console.log(show)
+    }
+  
+  
+    return (
+        <>
+        <div>
+
+            <div className="container1">
+
+                <div className="main">
+
+                    <div className="main-left">
+
+                        <p className="facebook-logo">facebook</p>
+
+                        <p className="facebook-status">Facebook helps you connect and share with the people in your life.</p>
+
+                    </div>
+
+                    <div className="main-right">
+
+                        <div className="main-right-login">
+
+                            <div className="main-right-email">
+
+                                <input type="email" placeholder="Email address or phone number"/>
+
+                            </div>
+
+                            <div className="main-right-password">
+
+                                <input type="password" placeholder="Password"/>
+
+                            </div>
+
+                            <div className="main-right-button">
+
+                                <button>
+                                    Log In
+                                </button>
+
+                            </div>
+
+                            <div className="main-right-link">
+
+                                <a href="/">Forgotten account?</a>
+
+                            </div>
+
+                            <div className="main-right-line">
+
+                            </div>
+
+                            <div className="main-right-account">
+
+                                <button id="signup-account" onClick={handleShow}>Create New Account</button>
+
+                            </div>
+
+                            <div className="main-right-page-link">
+
+                                <a href="/">create a Page </a><p>for a celebrity, band or business.</p>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+              </div>
+   
+             
+        
+          </div>
+          <div>
+       <div className={(show)?"modal-show":"modal-hide"}>
+       <form onSubmit={createAccount}>
+          
+          <div className="modal-signup">
+             
+             <div className="modal-close" onClick={handleShow}>
+                 X
+             </div>
+             
+             <div className="modal-signup-heading">
+                 
+                 <p className="modal-signup-name">Sign Up</p>
+                 
+                 <p className="modal-signup-child-name">It's quick and easy.</p>
+                 
+             </div>
+             
+             <div className="modal-signup-name">
+                       
+                <input type="text" placeholder="First name"/>
+                
+                <input type="text" placeholder="Surname"/>
+                           
+             </div>
+             
+             <div className="modal-signup-email">
+                
+                       
+                <input type="email" placeholder="Email address or phone number" onChange={
+                    getUserEmail
+                    
+                }  />
+                       
+             </div>
+                   
+             <div className="modal-signup-password">
+                       
+                 <input type="password" placeholder="Password" onChange={
+                    getUserPass
+                 }/>
+                       
+              </div>
+          
+              <div className="modal-date-birth">
+                 
+                 <label for="">Date of birth</label>
+                 
+                 <div className="modal-date-alert">
+                     
+                     <a >&#63;</a>
+                     
+                 </div>
+                  
+              </div>
+              
+              <div className="modal-date-selection">
+                  
+                   
+              </div>
+              
+              <div className="modal-gender">
+                 
+                 <label for="">Gender</label>
+                 
+                 <div className="modal-gender-alert">
+                     
+                     <a >&#63;</a>
+                     
+                 </div>
+                  
+              </div>
+              
+              <div className="modal-gender-choice">
+                 
+                 <div className="modal-gender-name">
+                     
+                     <label for="">Femal</label>
+                     
+                     <input type="radio"/>
+                     
+                 </div>
+                 
+                 <div className="modal-gender-name">
+                     
+                     <label for="">Male</label>
+                     
+                     <input type="radio"/>
+                     
+                 </div>
+                 
+                 <div className="modal-gender-name">
+                     
+                     <label for="">Custom</label>
+                     
+                     <input type="radio"/>
+                     
+                 </div>
+                  
+              </div>
+              
+              <div className="modal-signup-terms">
+                  
+                  <p> By clicking Sign Up, you agree to our Terms, Data Policy and Cookie Policy. You may receive SMS notifications from us and can opt out at any time.  
+                    </p>
+                    
+              </div>
+              
+              <div className="modal-signup-button">
+                  
+                  <button >Sign Up</button>
+                  
+              </div>
+              
+          </div>
+          </form>
+       </div>
+    </div>
+              
+          </>
+
+    
+        
+             
+            )
+}
